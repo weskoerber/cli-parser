b18efa74ba4087ca3e07df7963ade60b16caeed9
Wes Koerber <wes@weskoerber.com>
2023-11-09 01:29:06 -0500

    chore: add tests

---
+++ test/test_simple.c
@@ -0,0 +1,80 @@
+#include "cli/cli.h"
+
+#include <criterion/criterion.h>
+#include <criterion/new/assert.h>
+
+bool found = false;
+int num = -1;
+
+void reset(void) {
+  found = false;
+  num = -1;
+}
+
+void test_simple(size_t nargs, const char *args[]) {
+  found = true;
+}
+
+void test_arity_0(size_t nargs, const char *args[]) {
+  cr_assert(eq(sz, 1, nargs));
+  cr_assert(ne(ptr, nullptr, args));
+  cr_assert(ne(ptr, nullptr, args[0]));
+
+  num = strtol(args[0], nullptr, 10);
+}
+
+Test(cli, arity_none, .init = reset) {
+  int argc = 2;
+  const char *argv[] = {
+    "appname",
+    "-t",
+  };
+  ParseError err;
+
+  cli_init();
+  cli_add_option("-t", 0, test_simple);
+  err = cli_parse(argc, argv);
+  cli_cleanup();
+
+  cr_assert(eq(int, PARSE_ERROR_NONE, err));
+  cr_assert(found);
+}
+
+Test(cli, arity_1, .init = reset) {
+  int argc = 3;
+  const char *argv[] = {
+    "appname",
+    "-n",
+    "42",
+  };
+  ParseError err;
+
+  cli_init();
+  cli_add_option("-n", 1, test_arity_0);
+  err = cli_parse(argc, argv);
+  cli_cleanup();
+
+  cr_assert(eq(int, PARSE_ERROR_NONE, err));
+  cr_assert(eq(int, 42, num));
+}
+
+Test(cli, multiple_args, .init = reset) {
+  int argc = 4;
+  const char *argv[] = {
+    "appname",
+    "-t",
+    "-n",
+    "42",
+  };
+  ParseError err;
+
+  cli_init();
+  cli_add_option("-t", 0, test_simple);
+  cli_add_option("-n", 1, test_arity_0);
+  err = cli_parse(argc, argv);
+  cli_cleanup();
+
+  cr_assert(eq(int, PARSE_ERROR_NONE, err));
+  cr_assert(found);
+  cr_assert(eq(int, 42, num));
+}
